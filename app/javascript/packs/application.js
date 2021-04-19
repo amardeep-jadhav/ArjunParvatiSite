e2b09bd862c633481b1cd3e88263b47bf38c5dcd
jayakumar.krishnamoorthy <jayakumar.krishnamoorthy@medecision.com>
2021-04-19 16:54:02 +0530

    added some references for after login Layout structure implementation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,10 +11,23 @@ import "channels"
 import "bootstrap"
 import "@fortawesome/fontawesome-free/css/all"
 import "../src/core/jquery.3.2.1.min";
-import "../src/core/bootstrap.min";
 import "../src/core/popper.min";
-import "../src/atlantis.min";
+import "../src/core/bootstrap.min";
 import "../src/plugin/jquery-ui-1.12.1.custom/jquery-ui.min";
+import "../src/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min";
+import "../src/plugin/jquery-scrollbar/jquery.scrollbar.min";
+import "../src/plugin/chart.js/chart.min";
+import "../src/plugin/jquery.sparkline/jquery.sparkline.min";
+import "../src/plugin/chart-circle/circles.min";
+import "../src/plugin/datatables/datatables.min";
+import "../src/plugin/bootstrap-notify/bootstrap-notify.min";
+import "../src/plugin/jqvmap/jquery.vmap.min";
+import "../src/plugin/jqvmap/maps/jquery.vmap.world";
+import "../src/plugin/sweetalert/sweetalert.min";
+import "../src/plugin/webfont/webfont.min";
+import "../src/atlantis.min";
+import "../src/demo";
+
 
 // import 'src/stylesheets/application'
 // const images = require.context('../images', true)
